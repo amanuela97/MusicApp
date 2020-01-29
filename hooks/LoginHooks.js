caca13e3120581aeaf2e2708f012f798638f887a
amanuela97 <babyzewdie@gmail.com>
2020-01-29 21:43:01 +0200

    validation and toggle added

--- hooks/LoginHooks.js
+++ hooks/LoginHooks.js
@@ -1,7 +1,71 @@
 import {useState} from 'react';
+const validation = {
+    confirmPassword: {
+        equality: "password"
+    },
+    username: {
+        presence: {
+            message: '^Please enter a username address'
+        },
+        length: {
+            minimum: 3,
+            message: '^Your username must be at least 3 characters'
+        },
+        format: {
+            pattern: "[a-z0-9]+",
+            flags: "i",
+            message: "can only contain a-z and 0-9"
+        }
+    },
+
+    email: {
+        presence: {
+            message: '^Please enter an email address'
+        },
+        email: {
+            pattern: '^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$',
+            message: '^Please enter a valid email address'
+        }
+    },
+
+    password: {
+        presence: {
+            message: '^Please enter your password'
+        },
+        length: {
+            minimum: 5,
+            message: '^Your password must be at least 5 characters'
+        }
+    },
+
+    full_name: {
+        presence: {
+            message: '^Please enter your fullName'
+        },
+        length: {
+            minimum: 5,
+            message: '^Your fullName must be at least 5 characters'
+        },
+        format: {
+            pattern: '^[a-zA-Z]{4,}(?: [a-zA-Z]+){0,2}$',
+            flags: "i",
+            message: "^Write a valid fullName"
+        }
+    },
+};
 
 const useSignUpForm = () => {
+    const emptyError = {
+        username: undefined,
+        email: undefined,
+        password: undefined,
+        confirmPassword: undefined,
+        full_name: undefined,
+        fetch: undefined,
+    };
     const [inputs, setInputs] = useState({});
+    const [errors, setError] = useState(emptyError);
+
     const handleUsernameChange = (text) => {
         setInputs((inputs) =>
             ({
@@ -36,8 +100,10 @@ const useSignUpForm = () => {
         handleEmailChange,
         handleFullNameChange,
         inputs,
+        errors,
+        setError,
     };
 };
 
 
-export default useSignUpForm;
+export {useSignUpForm, validation};
